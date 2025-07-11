98fa58b622a3f5817ce613e2154af77add4adf55
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-07-11 15:22:56 -0400

    Fix softmax

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -1,7 +1,8 @@
 import torch
 
+
 def softmax(x: torch.Tensor, i: int) -> torch.Tensor:
     max_val = torch.max(x, dim=i, keepdim=True).values
     stab = x - max_val
-    exp = torch.exp(x)
+    exp = torch.exp(stab)
     return exp / torch.sum(exp, dim=i, keepdim=True)
