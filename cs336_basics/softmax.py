959bf1e0adca3c9de54536d1304396c15e2c4678
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-07-15 10:32:31 -0400

    Add temperature

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -1,8 +1,8 @@
 import torch
 
 
-def softmax(x: torch.Tensor, i: int) -> torch.Tensor:
+def softmax(x: torch.Tensor, i: int, temp: float = 1.0) -> torch.Tensor:
     max_val = torch.max(x, dim=i, keepdim=True).values
     stab = x - max_val
-    exp = torch.exp(stab)
+    exp = torch.exp(stab / temp)
     return exp / torch.sum(exp, dim=i, keepdim=True)
